f982eeea6b85f6d5770eb79d30c1af79926ccfc8
Nassim Arifette <nassim.ari@gmail.com>
2025-12-26 12:55:15 +0100

    modification robot.txt

--- app/robots.ts
+++ app/robots.ts
@@ -6,8 +6,7 @@ export default function robots(): MetadataRoute.Robots {
 
   return {
     rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: [`${siteUrl}/sitemap.xml`],
-    host: siteUrl,
+    sitemap: [`${siteUrl}/sitemap.xml`]
   }
 }
 
